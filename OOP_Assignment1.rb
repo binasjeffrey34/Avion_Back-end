3c86d717014a0234f82be05efe243f6b023fc250
Jeffrey Biñas <binasjeffrey34@yahoo.com>
2024-01-26 21:18:15 +0800

    OOP_Assignment1

---
+++ OOP_Assignment1.rb
@@ -0,0 +1,14 @@
+
+class Revenue
+    def initialize(no_of_calls, total_sales)
+        @no_of_calls = no_of_calls
+        @total_sales = total_sales
+    end
+
+    def Revenue_Per_Call
+        @total_sales / @no_of_calls
+    end
+end
+
+revenue = Revenue.new(500, 14000)
+puts revenue.Revenue_Per_Call
